e36c9486690597f721569de6d9dc25b7127e7d15
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-03-04 16:38:05 -0600

    Initial structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+import sys
+import os
+try: from setuptools import setup
+except ImportError: from distutils.core import setup
+import versioneer
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(filename):
+    return open(os.path.join(here,filename)).read()
+
+setup(
+    name='toolbelt',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/kadrlica/toolbelt',
+    author='Alex Drlica-Wagner',
+    author_email='kadrlica@fnal.gov',
+    scripts = [],
+    install_requires=[
+        'python >= 2.7.0',
+    ],
+    packages=['toolbelt'],
+    description="A place to hold python tools.",
+    long_description=read('README.md'),
+    platforms='any',
+    keywords='python tools',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+    ]
+)
